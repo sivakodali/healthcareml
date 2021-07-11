266be854b70dbf01ced770e18cad228ecf85e18a
sivakodali <29611589+sivakodali@users.noreply.github.com>
2021-07-11 18:21:12 -0400

    Adding Streamlit app files to deploy to Heroku

---
+++ second_app.py
@@ -0,0 +1,30 @@
+from matplotlib import pyplot as plt
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn import tree
+# Prepare the data data
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+clf = DecisionTreeClassifier(random_state=1234)
+model = clf.fit(X, y)
+
+fig = plt.figure(figsize=(25,20))
+_ = tree.plot_tree(clf, 
+                   feature_names=iris.feature_names,  
+                   class_names=iris.target_names,
+                   filled=True)
+
+import graphviz
+# DOT data
+dot_data = tree.export_graphviz(clf, out_file=None, 
+                                feature_names=iris.feature_names,  
+                                class_names=iris.target_names,
+                                filled=True)
+
+# Draw graph
+graph = graphviz.Source(dot_data, format="png") 
+graph
+
+graph.render("decision_tree_graphivz")
